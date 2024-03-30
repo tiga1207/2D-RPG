ea615e4c44232e2796ed2fa25efdfd5e3ae6caae
김승태 <tiga1207@naver.com>
2024-03-30 21:11:28 +0900

    0330_2
    move and jump

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -10,7 +10,17 @@ public class Player : MonoBehaviour
     private float xInput; 
 
     private Animator anim;
-    // [SerializeField]private bool isMoving;
+
+    [Header("Collision info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+
+
+
+
+    private int facingDir =1;
+    private bool facingRight =true;   
 
 
     void Start()
@@ -24,14 +34,31 @@ public class Player : MonoBehaviour
     {
         Movement();
         CheckInput();
+        CollisionCheck();
+
+        Debug.Log(isGrounded);
         AnimatorController();
+        FlipController();
+
+
 
     }
 
-//
+    private void CollisionCheck()
+    {
+
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+
+        //캡슐 콜라이더로 체크할 경우 아래 코드
+        // isGrounded = Physics2D.Raycast(transform.position, Vector2.down, capsuleCollider2D.size.y/2f +0.5f, whatIsGround);
+
+    }
+
+    //
     private void CheckInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
+        
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -48,15 +75,45 @@ public class Player : MonoBehaviour
     //점프
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+        if(isGrounded)
+            {
+            rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+            }
     }
 
     //캐릭터 애니메이션
     private void AnimatorController()
     {
+        anim.SetFloat("yVelopcity",rb.velocity.y);
+
         //움직임 관련 애니메이션
         bool isMoving = rb.velocity.x != 0;
         anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isGrounded",isGrounded);
     }
+
+    private void Filp(){
+        facingDir = facingDir*-1;
+        facingRight= !facingRight;
+        transform.Rotate(0,180,0);
+    }
+
+    private void FlipController(){
+        if(rb.velocity.x>0 && !facingRight){
+            Filp();
+        }
+        else if(rb.velocity.x<0 && facingRight){
+            Filp();
+        }
+    }
+
+ 
+    private void OnDrawGizmos() {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
+    }
+
+
+
 }
+
   
