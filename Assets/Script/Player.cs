c8dc08d4d08103fc361cd637880b4b870abfafda
김승태 <tiga1207@naver.com>
2024-04-12 02:14:17 +0900

    0412
    0412

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,18 +1,24 @@
- using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{   
+{
     private Rigidbody2D rb;
-     [SerializeField]private  float moveSpeed;
-    [SerializeField]private float JumpForce;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float JumpForce;
 
     [Header("Dash")]
-    [SerializeField] private float DashSpeed;
-    [SerializeField]private float DashDuration;
-    [SerializeField]private float DashTime;
-    private float xInput; 
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashDuration;
+    private float dashTime;
+    [SerializeField] private float dashCooldown;
+    private float dashCooldownTimer;
+
+
+    [Header("Attack Info")]
+    [SerializeField]private bool isAttacking;
+    private int comboCounter;
+
+    private float xInput;
 
     private Animator anim;
 
@@ -24,15 +30,15 @@ public class Player : MonoBehaviour
 
 
 
-    private int facingDir =1;
-    private bool facingRight =true;   
+    private int facingDir = 1;
+    private bool facingRight = true;
 
 
     void Start()
     {
-        rb=GetComponent<Rigidbody2D>();
-        anim=GetComponentInChildren<Animator>();
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+
     }
 
     void Update()
@@ -41,24 +47,25 @@ public class Player : MonoBehaviour
         CheckInput();
         CollisionCheck();
 
-        //DashTime = DashTime - Time.deltaTime;
-        DashTime -= Time.deltaTime;
+        dashTime -= Time.deltaTime;
+        dashCooldownTimer -= Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            DashTime = DashDuration;
-        }
 
-        if(DashTime > 0)
+        if (dashTime > 0)
         {
             Debug.Log("I'm doing dash ability");
         }
-       
+
         AnimatorController();
         FlipController();
 
 
 
+    }
+
+    public void AttackOver()
+    {
+        isAttacking=false;
     }
 
     private void CollisionCheck()
@@ -75,24 +82,41 @@ public class Player : MonoBehaviour
     private void CheckInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0)){
+            isAttacking=true;
+        }
+
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            DashAbility();
+        }
+
+    }
+
+    private void DashAbility()
+    {
+        if (dashCooldownTimer < 0)
+        {
+            dashCooldownTimer = dashCooldown;
+            dashTime = dashDuration;
         }
     }
 
     //좌우 움직임 컨트롤
     private void Movement()
     {
-        if(DashTime>0)
+        if (dashTime > 0)
         {
-            //rb.velocity = new Vector2(xInput * DashSpeed, rb.velocity.y);
-            rb.velocity = new Vector2(xInput * DashSpeed, 0);
+            //rb.velocity = new Vector2(xInput * dashSpeed, rb.velocity.y);
+            rb.velocity = new Vector2(xInput * dashSpeed, 0);
         }
-        else 
-        { 
+        else
+        {
             rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
         }
 
@@ -101,41 +125,50 @@ public class Player : MonoBehaviour
     //점프
     private void Jump()
     {
-        if(isGrounded)
-            {
+        if (isGrounded)
+        {
             rb.velocity = new Vector2(rb.velocity.x, JumpForce);
-            }
+        }
     }
 
     //캐릭터 애니메이션
     private void AnimatorController()
     {
-        anim.SetFloat("yVelopcity",rb.velocity.y);
+        anim.SetFloat("yVelopcity", rb.velocity.y);
 
         //움직임 관련 애니메이션
         bool isMoving = rb.velocity.x != 0;
         anim.SetBool("isMoving", isMoving);
-        anim.SetBool("isGrounded",isGrounded);
-        anim.SetBool("isDashing",DashTime>0);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isDashing", dashTime > 0);
+        anim.SetBool("isAttacking", isAttacking);
+        anim.SetInteger("comboCounter", comboCounter);
+
+
     }
 
-    private void Filp(){
-        facingDir = facingDir*-1;
-        facingRight= !facingRight;
-        transform.Rotate(0,180,0);
+    private void Filp()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
     }
 
-    private void FlipController(){
-        if(rb.velocity.x>0 && !facingRight){
+    private void FlipController()
+    {
+        if (rb.velocity.x > 0 && !facingRight)
+        {
             Filp();
         }
-        else if(rb.velocity.x<0 && facingRight){
+        else if (rb.velocity.x < 0 && facingRight)
+        {
             Filp();
         }
     }
 
- 
-    private void OnDrawGizmos() {
+
+    private void OnDrawGizmos()
+    {
         Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
     }
 
@@ -143,4 +176,3 @@ public class Player : MonoBehaviour
 
 }
 
-  
